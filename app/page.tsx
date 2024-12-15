1b562e552a9fd0c71c311c404bf50dd19fb756c9
Jomo178 <shahbazi8@outlook.de>
2024-12-15 13:04:03 +0100

    refactor: simplify Home component and update user fetching logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,22 @@
-"use client";
-
 import { Suspense } from "react";
-import { updateJsonFile } from "@/fix";
 
 import { mainNavigation } from "@/config/navigation";
-import { prisma } from "@/lib/database";
 import { getCurrentUser } from "@/lib/session";
-import { Button } from "@/components/ui/button";
 import Confettis from "@/components/ui/confettis";
 import Navbar from "@/components/navbar";
 
-export default function Home() {
-  // const user = await getCurrentUser();
+export default async function Home() {
+  const user = await getCurrentUser();
 
   return (
     <div>
       <Confettis />
-      {/* <Suspense>
+      <Suspense>
         <Navbar
           user={{ global_name: user?.global_name, image: user?.image }}
           navigationItems={mainNavigation}
         />
-      </Suspense> */}
-      <Button onClick={() => updateJsonFile()}>Click me</Button>
+      </Suspense>
     </div>
   );
 }
